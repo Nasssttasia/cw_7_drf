d1abeb76e3dacadd70635fdf5bbc7ce4b30e6e6b
Anastasia Chernysheva <nastya770098@gmail.com>
2023-12-20 21:32:03 +0300

    Initial commit

---
+++ users/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {'null': True, 'blank': True}
+
+class User(AbstractUser):
+    class User(AbstractUser):
+        username = None
+        email = models.EmailField(unique=True, verbose_name='почта')
+        name = models.CharField(max_length=50, verbose_name='имя', **NULLABLE)
+
+        USERNAME_FIELD = "email"
+        REQUIRED_FIELDS = []
+
